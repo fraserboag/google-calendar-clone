30d82764ff017e9047c7b4c0a6be15937d9e4de9
Fraser Boag <fraser.boag@verint.com>
2024-03-07 22:42:28 +0000

    progress on calendar

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -11,6 +11,7 @@ import {
   isToday,
   subMonths,
   addMonths,
+  isSameDay,
 } from "date-fns"
 import { formatDate } from "../utils/formatDate"
 import { cc } from "../utils/cc"
@@ -27,6 +28,8 @@ export function Calendar() {
     const lastWeekEnd = endOfWeek(endOfMonth(selectedMonth))
     return eachDayOfInterval({ start: firstWeekStart, end: lastWeekEnd })
   }, [selectedMonth])
+
+  const { events } = useEvents()
 
   return (
     <div className="calendar">
@@ -46,7 +49,13 @@ export function Calendar() {
       </div>
       <div className="days">
         {calendarDays.map((day, index) => (
-          <CalendarDay key={day.getTime()} day={day} showWeekDay={index < 7} selectedMonth={selectedMonth} />
+          <CalendarDay
+            key={day.getTime()}
+            day={day}
+            showWeekDay={index < 7}
+            selectedMonth={selectedMonth}
+            events={events.filter((event) => isSameDay(day, event.date))}
+          />
         ))}
       </div>
     </div>
@@ -57,11 +66,28 @@ type CalendarDayProps = {
   day: Date
   showWeekDay: boolean
   selectedMonth: Date
+  events: Event[]
 }
 
-function CalendarDay({ day, showWeekDay, selectedMonth }: CalendarDayProps) {
+function CalendarDay({ day, showWeekDay, selectedMonth, events }: CalendarDayProps) {
   const [isNewEventModalOpen, setIsNewEventModalOpen] = useState(false)
   const { addEvent } = useEvents()
+
+  const sortedEvents = useMemo(() => {
+    const timeToNumber = (time: string) => parseFloat(time.replace(":", "."))
+
+    return [...events].sort((a, b) => {
+      if (a.allDay && b.allDay) {
+        return 0
+      } else if (a.allDay) {
+        return -1
+      } else if (b.allDay) {
+        return 1
+      } else {
+        return timeToNumber(a.startTime) - timeToNumber(b.startTime)
+      }
+    })
+  }, [events])
 
   return (
     <div
@@ -78,6 +104,21 @@ function CalendarDay({ day, showWeekDay, selectedMonth }: CalendarDayProps) {
           +
         </button>
       </div>
+      {sortedEvents.length > 0 && (
+        <div className="events">
+          <button className="all-day-event blue event">
+            <div className="event-name">Short</div>
+          </button>
+          <button className="all-day-event green event">
+            <div className="event-name">Long Event Name That Just Keeps Going</div>
+          </button>
+          <button className="event">
+            <div className="color-dot blue"></div>
+            <div className="event-time">7am</div>
+            <div className="event-name">Event Name</div>
+          </button>
+        </div>
+      )}
       <EventFormModal
         date={day}
         isOpen={isNewEventModalOpen}
